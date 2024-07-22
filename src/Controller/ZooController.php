6c48a5ea2a5ba6651bc5da612021e346beae4767
MbarkaToure <mbarkatoure04@gmail.com>
2024-07-22 20:53:49 +0200

    Documenter son API

--- src/Controller/ZooController.php
+++ src/Controller/ZooController.php
@@ -7,6 +7,7 @@ use App\Repository\ZooRepository;
 use DateTimeImmutable;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
+use OpenApi\Annotations as OA;
 use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Exception\BadRequestException;
@@ -33,6 +34,34 @@ class ZooController extends AbstractController
         
     }
     #[Route(methods:'POST')]
+
+
+    /** @OA\Get(
+     *     path="/api/zoo",
+     *     summary="Créer un zoo ",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         description="Donner du zoo à créer",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="name", type="string", example="Nom du Zoo"),
+     *             @OA\Property(property="description", type="string", example="Description du Zoo")
+     *         )
+     * 
+     *     ),
+     *     @OA\Response(
+     *         response=261,
+     *         description="Zoo crée avec succès",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="id", type="integer", example=1),
+     *             @OA\Property(property="name", type="string", example="Nom du Zoo"),
+     *             @OA\Property(property="description", type="string", example="Description du Zoo"),
+     *             @OA\Property(property="createdAt", type="string", format="date-time")
+     *         )
+     *     )
+     *     )
+     */
+
     public function new(Request $request): JsonResponse
 {
     $zoo = $this->serializer->deserialize($request->getContent(), Zoo::class, 'json');
@@ -55,6 +84,35 @@ class ZooController extends AbstractController
 }
 
     #[Route('/{id}',name: 'show', methods:'GET')]
+
+    /** @OA\Get(
+     *     path="/api/zoo/{id}",
+     *     summary="Afficher un zoo par son ID",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         description="ID du zoo à afficher",
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Zoo trouvé avec succès",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="id", type="integer", example=1),
+     *             @OA\Property(property="name", type="string", example="Nom du Zoo"),
+     *             @OA\Property(property="description", type="string", example="Description du Zoo"),
+     *             @OA\Property(property="createdAt", type="string", format="date-time")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Zoo non trouvé"
+     *     )
+     * )
+     */
+
     public function show(int $id): JsonResponse
 {
     $zoo = $this->repository->findOneBy(['id' => $id]);
@@ -70,6 +128,37 @@ class ZooController extends AbstractController
 }
 
     #[Route('/{id}',name:'edit', methods:'PUT')]
+
+/** 
+ * @OA\Put(
+ *     path="/api/zoo/{id}",
+ *     summary="Mettre à jour un zoo par son ID",
+ *     @OA\Parameter(
+ *         name="id",
+ *         in="path",
+ *         required=true,
+ *         description="ID du zoo à mettre à jour",
+ *         @OA\Schema(type="integer")
+ *     ),
+ *     @OA\RequestBody(
+ *         required=true,
+ *         @OA\JsonContent(
+ *             type="object",
+ *             @OA\Property(property="name", type="string", example="Nom du Zoo"),
+ *             @OA\Property(property="description", type="string", example="Description du Zoo")
+ *         )
+ *     ),
+ *     @OA\Response(
+ *         response=204,
+ *         description="Zoo mis à jour avec succès"
+ *     ),
+ *     @OA\Response(
+ *         response=404,
+ *         description="Zoo non trouvé"
+ *     )
+ * )
+ */
+
     public function edit(int $id, Request $request): JsonResponse
 {
     $zoo = $this->repository->findOneBy(['id' => $id]);
@@ -92,6 +181,29 @@ class ZooController extends AbstractController
 }
 
     #[Route('/{id}',name:'delete', methods:'DELETE')]
+
+/** 
+ * @OA\Delete(
+ *     path="/api/zoo/{id}",
+ *     summary="Supprimer un zoo par son ID",
+ *     @OA\Parameter(
+ *         name="id",
+ *         in="path",
+ *         required=true,
+ *         description="ID du zoo à supprimer",
+ *         @OA\Schema(type="integer")
+ *     ),
+ *     @OA\Response(
+ *         response=204,
+ *         description="Zoo supprimé avec succès"
+ *     ),
+ *     @OA\Response(
+ *         response=404,
+ *         description="Zoo non trouvé"
+ *     )
+ * )
+ */
+
     public function delete(int $id): JsonResponse
 {
     $zoo = $this->repository->findOneBy(['id' => $id]);
